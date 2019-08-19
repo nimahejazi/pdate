359bbd8f5de3e66257c610d879205e6d2b50ccb2
Nima Hejazi <nhejazi@gmail.com>
2019-08-19 11:37:16 +0430

    Add make_tm and make_tm_cur

--- fadate.c
+++ fadate.c
@@ -1,5 +1,8 @@
+#define _XOPEN_SOURCE
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <stdio.h>
 
 int split_date_str(char *date, int *year, int *mon, int *day, char *splitter)
 {
@@ -14,6 +17,21 @@ int split_date_str(char *date, int *year, int *mon, int *day, char *splitter)
   *mon = atoi(_mon);
   *day = atoi(_day);
 
-
   return 0;
+}
+
+struct tm *make_tm(int year, int mon, int day, struct tm *ltm)
+{
+  char strtime[11];
+  sprintf(strtime, "%4d-%02d-%02d", year, mon, day);
+  strptime(strtime, "%Y-%m-%d", ltm);
+  return ltm;
+}
+
+struct tm *make_tm_cur(struct tm *ltm)
+{
+  time_t cur_date;
+  time(&cur_date);
+  ltm = localtime(&cur_date);
+  return ltm;
 }
--- fadate.h
+++ fadate.h
@@ -1,1 +1,3 @@
 int split_date_str(char *date, int *year, int *mon, int *day, char *splitter);
+struct tm *make_tm(int year, int mon, int day, struct tm *ltm);
+struct tm *make_tm_cur(struct tm *ltm);
