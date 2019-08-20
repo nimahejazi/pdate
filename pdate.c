dfe31297f5710653129871dc6c81637356c368bf
Nima Hejazi <nima@Nimas-MacBook-Pro.local>
2019-08-20 17:29:39 +0430

    Add Persian to Georgian function, not complete
    Persian to Georgian function works correctly, now it only returns
    a string, instead of 'struct tm'.

--- pdate.c
+++ pdate.c
@@ -6,14 +6,4 @@
 
 int main(int argc, char **argv)
 {
-  int year, mon, day;
-  while (1)
-  {
-    printf("\n----------------\n");
-    printf("Enter date: YYYY MM DD\n");
-    scanf("%d %d %d", &year, &mon, &day);
-    struct tm *tml = malloc(sizeof(struct tm));
-    convert_to_georgian(year, mon, day, tml);
-    free(tml);
-  }
 }
